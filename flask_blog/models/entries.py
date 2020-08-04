fdfacf339d547630e82cce3426f9bbedcb31f03b
wizard-laplace <54266481+wizard-Laplace@users.noreply.github.com>
2020-08-04 13:56:32 +0900

    Modelの定義

---
+++ flask_blog/models/entries.py
@@ -0,0 +1,20 @@
+# flask_blog/models/entries.py
+
+from flask_blog import db
+from datetime import datetime
+
+class Entry(db.Model):
+    """ Entityクラス """
+    __tablename__ = 'entries'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.string(50), unique=True)
+    text = db.Column(db.Text)
+    created_at = db.Column(db.Datetime)
+
+    def __init__(self, title=None, text=None):
+        self.title = title
+        self.text = text
+        self.created_at = datetime.utcnow()
+
+    def __repr__(self):
+        return '<Entry id:{} title:{} text:{}>'.format(self.id, self.title, self.text)
