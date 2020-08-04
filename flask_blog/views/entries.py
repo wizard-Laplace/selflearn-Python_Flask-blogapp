cb632c03c119b5283bd09701b7b8c700d9575331
wizard-laplace <54266481+wizard-Laplace@users.noreply.github.com>
2020-08-04 15:04:27 +0900

    new_entry用のビューを追加

--- flask_blog/views/entries.py
+++ flask_blog/views/entries.py
@@ -2,6 +2,7 @@
 
 from flask import request, redirect, url_for, render_template, flash, session
 from flask_blog import app
+from flask_blog.models.entries import Entry
 
 @app.route('/')
 def show_entries():
@@ -9,3 +10,10 @@ def show_entries():
     if not session.get('logged_in'):
         return redirect(url_for('login'))
     return render_template('entries/index.html')
+
+@app.route('/entries/new', methods=['GET'])
+def new_entry():
+    """ new_entryビューを追加 """
+    if not session.get('logged_in'):
+        return redirect(url_for('login'))
+    return render_template('entries/new.html')
