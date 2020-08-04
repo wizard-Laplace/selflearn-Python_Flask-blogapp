79f35b1a1cc1a81fdb4b07d3b57abea0d7f43ad5
wizard-laplace <54266481+wizard-Laplace@users.noreply.github.com>
2020-08-04 15:40:11 +0900

    show_entryビューで全ての記事をdbから取得

--- flask_blog/views/entries.py
+++ flask_blog/views/entries.py
@@ -2,22 +2,16 @@
 
 from flask import request, redirect, url_for, render_template, flash, session
 from flask_blog import app
-from flask_blog.models.entries import Entry
 from flask_blog import db
+from flask_blog.models.entries import Entry
 
 @app.route('/')
 def show_entries():
     """ ログインしていない時の処理 session['logged_in'] = Falseの場合 """
     if not session.get('logged_in'):
         return redirect(url_for('login'))
-    return render_template('entries/index.html')
-
-@app.route('/entries/new', methods=['GET'])
-def new_entry():
-    """ new_entryビューを追加 """
-    if not session.get('logged_in'):
-        return redirect(url_for('login'))
-    return render_template('entries/new.html')
+    entries = Entry.query.order_by(Entry.id.desc()).all()
+    return render_template('entries/index.html', entries=entries)
 
 @app.route('/entries', methods=['POST'])
 def add_entry():
@@ -32,3 +26,11 @@ def add_entry():
     db.session.commit()
     flash('新しく記事が作成されました')
     return redirect(url_for('show_entries'))
+
+@app.route('/entries/new', methods=['GET'])
+def new_entry():
+    """ new_entryビューを追加 """
+    if not session.get('logged_in'):
+        return redirect(url_for('login'))
+    return render_template('entries/new.html')
+
