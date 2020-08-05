9b7c06874f47f2be3fe6af5bbfa57a8aee8575ba
wizard-laplace <54266481+wizard-Laplace@users.noreply.github.com>
2020-08-05 13:59:05 +0900

    ログイン確認ロジックのデコレータ作成とデコレータ利用

--- flask_blog/views/entries.py
+++ flask_blog/views/entries.py
@@ -4,19 +4,18 @@ from flask import request, redirect, url_for, render_template, flash, session
 from flask_blog import app
 from flask_blog import db
 from flask_blog.models.entries import Entry
+from flask_blog.views.views import login_required
 
 @app.route('/')
+@login_required
 def show_entries():
     """ ログインしていない時の処理 session['logged_in'] = Falseの場合 """
-    if not session.get('logged_in'):
-        return redirect(url_for('login'))
     entries = Entry.query.order_by(Entry.id.desc()).all()
     return render_template('entries/index.html', entries=entries)
 
 @app.route('/entries', methods=['POST'])
+@login_required
 def add_entry():
-    if not session.get('logged_in'):
-        return redirect(url_for('login'))
     entry = Entry(
         title=request.form['title'],
         text=request.form['text']
@@ -28,30 +27,27 @@ def add_entry():
     return redirect(url_for('show_entries'))
 
 @app.route('/entries/new', methods=['GET'])
+@login_required
 def new_entry():
     """ new_entryビューを追加 """
-    if not session.get('logged_in'):
-        return redirect(url_for('login'))
+
     return render_template('entries/new.html')
 
 @app.route('/entries/<int:id>', methods=['GET'])
+@login_required
 def show_entry(id):
-    if not session.get('logged_in'):
-        return redirect(url_for('login'))
     entry = Entry.query.get(id)
     return render_template('entries/show.html', entry=entry)
 
 @app.route('/entries/<int:id>/edit', methods=['GET'])
+@login_required
 def edit_entry(id):
-    if not session.get('logged_in'):
-        return redirect(url_for('login'))
     entry = Entry.query.get(id)
     return render_template('entries/edit.html', entry=entry)
 
 @app.route('/entries/<int:id>/update', methods=['POST'])
+@login_required
 def update_entry(id):
-    if not session.get('logged_in'):
-        return redirect(url_for('login'))
     entry = Entry.query.get(id)
     entry.title = request.form['title']
     entry.text = request.form['text']
@@ -61,9 +57,8 @@ def update_entry(id):
     return redirect(url_for('show_entries'))
 
 @app.route('/entries/<int:id>/delete', methods=['POST'])
+@login_required
 def delete_entry(id):
-    if not session.get('logged_in'):
-        return redirect(url_for('login'))
     entry = Entry.query.get(id)
     db.session.delete(entry)
     db.session.commit()
